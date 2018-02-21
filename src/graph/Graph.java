953a860d218a54e62a2e8f0a47142e4dfcda0d03
Jack Maguire <jackmaguire1444@gmail.com>
2018-02-21 13:06:28 -0500

    Added functionality to Graph class

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -16,4 +16,19 @@ public class Graph implements GraphType {
 		return nodes_[ node_index ];
 	}
 	
+	public void addEdge( EdgeType edge ) {
+		nodes_[ edge.incomingNodeIndex() ].addUpstreamEdge( edge );
+		nodes_[ edge.outgoingNodeIndex() ].addDownstreamEdge( edge );
+	}
+	
+	public EdgeType getEdge( int outgoing_node_index, int incoming_node_index ) {
+		EdgeType[] edges = nodes_[ outgoing_node_index ].getDownstreamEdges();
+		for( EdgeType edge : edges ) {
+			if( edge.incomingNodeIndex() == incoming_node_index ) {
+				return edge;
+			}
+		}
+		return null;
+	}
+	
 }
--- src/graph/GraphType.java
+++ src/graph/GraphType.java
@@ -3,5 +3,8 @@ package graph;
 public interface GraphType {
 
 	NodeType getNode( int node_index );
+	void addNode( NodeType node, int node_index );
 	
+	EdgeType getEdge( int outgoing_node_index, int incoming_node_index );
+	void addEdge( EdgeType edge );
 }
