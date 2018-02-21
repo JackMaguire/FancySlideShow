235f535b90530faa6ddb8ca4e87b94594af7ba74
Jack Maguire <jackmaguire1444@gmail.com>
2018-02-21 12:36:06 -0500

    Added has_images() to interface for edges

--- src/graph/Edge.java
+++ src/graph/Edge.java
@@ -2,4 +2,8 @@ package graph;
 
 public class Edge implements EdgeType {
 
+	public boolean has_images() {
+		return false;
+	}
+	
 }
--- src/graph/EdgeType.java
+++ src/graph/EdgeType.java
@@ -2,4 +2,6 @@ package graph;
 
 public interface EdgeType {
 
+	boolean has_images();
+	
 }
