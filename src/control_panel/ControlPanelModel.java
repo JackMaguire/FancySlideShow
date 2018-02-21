3e3903b7d2d92a0e4de53e8382e1c780ef4e4c64
Jack Maguire <jackmaguire1444@gmail.com>
2018-02-21 13:47:50 -0500

    Added Models for east, center, and west

--- src/control_panel/ControlPanelModel.java
+++ src/control_panel/ControlPanelModel.java
@@ -2,4 +2,10 @@ package control_panel;
 
 public class ControlPanelModel {
 
+	private WestPanelModel west_;
+	private CenterPanelModel center_;
+	private EastPanelModel east_;
+	
+	
+
 }
---
+++ src/control_panel/WestPanelView.java
@@ -0,0 +1,7 @@
+package control_panel;
+
+import javax.swing.JPanel;
+
+public class WestPanelView extends JPanel {
+
+}
