65c13b13d68eb8dc85777433b910b3dc340a96a2
Jack Maguire <jackmaguire1444@gmail.com>
2018-02-21 15:57:50 -0500

    Updated Current Panel Model

--- src/control_panel/CenterPanelModel.java
+++ src/control_panel/CenterPanelModel.java
@@ -1,11 +1,30 @@
 package control_panel;
 
+import graph.*;
+
 public class CenterPanelModel {
 
-	private graph.Graph graph_;
+	private Graph graph_;
+
+	private EdgeType selected_edge_;
+	private NodeType current_node_;
 	
-	public graph.Graph getGraph(){
+	public CenterPanelModel( Graph graph ) {
+		graph_ = graph;
+		current_node_ = graph.getNode( 0 );
+		selected_edge_ = current_node_.getDownstreamEdges()[0];
+	}
+	
+	public Graph getGraph(){
 		return graph_;
 	}
 	
+	public EdgeType selectedEdge() {
+		return selected_edge_;
+	}
+	
+	public NodeType currentNode() {
+		return current_node_;
+	}
+
 }
--- src/control_panel/CenterPanelView.java
+++ src/control_panel/CenterPanelView.java
@@ -1,9 +1,62 @@
 package control_panel;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
+
 public class CenterPanelView {
 
 	CenterPanelModel model_;
 	
+	private EdgeLine[] lines;
+	private NodeCircle[] circles;
 	
+	private Color default_;
+	private Color forward_;
+	private Color backward_;
+	private Color selected_;
+	
+	private static class NodeCircle {
+		final public int x;
+		final public int y;
+		//final public int r;//defined outside
+		private Color color_;
+		
+		public NodeCircle( int x, int y ) {
+			this.x = x;
+			this.y = y;
+		}
+		
+		public void setColor( Color color ) {
+			color_ = color;
+		}
+		
+		public void draw( Graphics g, int diameter ) {
+			g.setColor( color_ );
+			g.fillOval(x, y, diameter, diameter);
+		}
+	}
+	
+	private static class EdgeLine {
+		final public int x1, y1, x2, y2;
+
+		private Color color_;
+		
+		public EdgeLine( int x1, int y1, int x2, int y2 ) {
+			this.x1 = x1;
+			this.y1 = y1;
+			this.x2 = x2;
+			this.y2 = y2;
+		}
+		
+		public void setColor( Color color ) {
+			color_ = color;
+		}
+		
+		public void draw( Graphics g ) {
+			g.setColor( color_ );
+			g.drawLine(x1, y1, x2, y2);
+		}
+	}
 	
 }
