a3f0d0b4f78d067125c28bbd3c0bab15204da77e
Jack Maguire <jackmaguire1444@gmail.com>
2018-02-21 12:01:37 -0500

    Added ctor and getter to SlideShow

--- src/slide_show/SlideShow.java
+++ src/slide_show/SlideShow.java
@@ -1,5 +1,19 @@
 package slide_show;
 
+import graph.*;
+
 public class SlideShow {
 
+	//Starts at Node 0
+	private Node[] nodes_;
+	
+	public SlideShow( Node[] nodes ) {
+		nodes_ = nodes;
+	}
+	
+	//Getters and setters
+	protected Node[] nodes() {
+		return nodes_;
+	}
+	
 }
