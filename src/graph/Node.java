2145df30076e9bc1592841bd85d795c74b51f928
Jack Maguire <jackmaguire1444@gmail.com>
2018-02-21 11:59:04 -0500

    Updated NodeType to return arrays of edges and images

--- src/graph/Node.java
+++ src/graph/Node.java
@@ -2,4 +2,12 @@ package graph;
 
 public class Node implements NodeType {
 
+	public EdgeType[] get_upstream_edges() {
+		return null;
+	}
+	
+	public EdgeType[] get_downstream_edges() {
+		return null;
+	}
+	
 }
--- src/graph/NodeType.java
+++ src/graph/NodeType.java
@@ -2,4 +2,9 @@ package graph;
 
 public interface NodeType {
 
+	EdgeType[] get_upstream_edges();
+	EdgeType[] get_downstream_edges();
+	
+	
+	
 }
