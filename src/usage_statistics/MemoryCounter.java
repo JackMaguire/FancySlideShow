b882a7b890a009d00833323342e5efa094ab1963
Jack Maguire <jackmaguire1444@gmail.com>
2018-03-08 12:05:21 -0500

    FormattedMemoryCounter

--- src/usage_statistics/MemoryCounter.java
+++ src/usage_statistics/MemoryCounter.java
@@ -1,28 +1,51 @@
 package usage_statistics;
+
 import java.util.*;
+
 public class MemoryCounter {
 
 	private final static MemoryCounter instance_ = new MemoryCounter();
-	
-	//private long bytes_for_buffered_images_; 
+
+	// private long bytes_for_buffered_images_;
 	private final Map< String, Long > bytes_for_token_ = new HashMap< String, Long >();
-	
-	public MemoryCounter() {}
-	
+
+	public MemoryCounter() {
+		Runtime.getRuntime().addShutdownHook( new shutdown_hook( bytes_for_token_ ) );
+	}
+
 	public static MemoryCounter getInstance() {
 		return instance_;
 	}
-	
+
 	public void addBytesForToken( String token, long bytes ) {
-		if( ! bytes_for_token_.containsKey( token ) ) {
+		if( !bytes_for_token_.containsKey( token ) ) {
 			bytes_for_token_.put( token, bytes );
-		} else {
+		}
+		else {
 			bytes_for_token_.put( token, bytes_for_token_.get( token ) + bytes );
 		}
 	}
-	
+
 	public long getBytesForToken( String token ) {
 		return bytes_for_token_.get( token );
 	}
-	
+
+	private static class shutdown_hook extends Thread {
+
+		private final Map< String, Long > bytes_for_token_;
+
+		public shutdown_hook( Map< String, Long > bytes_for_token ) {
+			bytes_for_token_ = bytes_for_token;
+		}
+
+		public void run() {
+			Iterator< ? > it = bytes_for_token_.entrySet().iterator();
+			System.out.println( "Memory usage:" );
+			while ( it.hasNext() ) {
+				Map.Entry pair = (Map.Entry) it.next();
+				System.out.println( "\t" + pair.getKey() + " = " + pair.getValue() );
+			}
+		}
+	}
+
 }
