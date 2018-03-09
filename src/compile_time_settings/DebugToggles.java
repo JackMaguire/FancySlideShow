801d955aa224ac11f49105d59bf1b31619cee2c5
Jack Maguire <jackmaguire1444@gmail.com>
2018-03-09 10:48:07 -0500

    toggled DEBUG_FRAME_GRAPH

--- src/compile_time_settings/DebugToggles.java
+++ src/compile_time_settings/DebugToggles.java
@@ -6,7 +6,7 @@ public class DebugToggles {
 
 	public final static boolean DEBUG_APPLICATIONS = DEBUG_ALL || false;
 	public final static boolean DEBUG_CONTROL_PANEL = DEBUG_ALL || false;
-	public final static boolean DEBUG_FRAME_GRAPH = DEBUG_ALL || true;
+	public final static boolean DEBUG_FRAME_GRAPH = DEBUG_ALL || false;
 	public final static boolean DEBUG_GRAPH = DEBUG_ALL || false;
 	public final static boolean DEBUG_SLIDE_SHOW = DEBUG_ALL || false;
 	public final static boolean DEBUG_VIEW = DEBUG_ALL || false;
