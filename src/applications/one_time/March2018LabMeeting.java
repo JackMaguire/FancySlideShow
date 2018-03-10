eed877e220024d7f43110323d55cca78d8a7cd51
Jack Maguire <jackmaguire1444@gmail.com>
2018-03-10 12:09:14 -0500

    Added MRS title nodes

--- src/applications/one_time/March2018LabMeeting.java
+++ src/applications/one_time/March2018LabMeeting.java
@@ -9,6 +9,10 @@ public class March2018LabMeeting {
 
 	private static String path_to_top_dir_ = "/Users/jackmaguire/Dropbox/March2018LabMeetingSlides";
 
+	//subgraphs
+	private static int TITLE_SUBGRAPH = 0;
+	private static int MRS_SUBGRAPH = 1;
+	
 	public static void main( String[] args ) {
 
 		if( args.length != 0 ) {
@@ -16,11 +20,17 @@ public class March2018LabMeeting {
 		}
 
 		final int num_nodes_for_title_slide = 4;
-		final int num_nodes_for_MRS_slide = 0;
+		final int num_nodes_for_MRS_slide = 2;
 		final int num_nodes = num_nodes_for_title_slide + num_nodes_for_MRS_slide;
 
 		Graph my_graph = new Graph( num_nodes, 2 );
-		setTitleSlideNodesAndEdges( my_graph, 0 );
+		int current_offset = 0;
+		
+		setTitleSlideNodesAndEdges( my_graph, current_offset );
+		current_offset += num_nodes_for_title_slide;
+		
+		setMRSNodesAndEdges( my_graph, current_offset );
+		current_offset += num_nodes_for_MRS_slide;
 		
 		SlideShow ss = new SlideShow( my_graph );
 		ss.run();
@@ -31,15 +41,15 @@ public class March2018LabMeeting {
 		//System.out.println( result );
 		return result;
 	}
-
+	
 	private static void setTitleSlideNodesAndEdges( Graph graph, int offset ) {
 
 		int node_id = offset;
 
-		graph.setNode( new Node( "Title", false, getFilename( "Title", 0 ), "Good Luck!" ), node_id++, 0 );
-		graph.setNode( new Node( "Title_mid", false, getFilename( "Title", 250 ), "" ), node_id++, 0 );
-		graph.setNode( new Node( "Title_final_start", false, getFilename( "Title", 501 ), "" ), node_id++, 0 );
-		graph.setNode( new Node( "Title_final_end", true, getFilename( "Title", 550 ), "" ), node_id++, 0 );
+		graph.setNode( new Node( "Title", false, getFilename( "Title", 0 ), "Good Luck!" ), node_id++, TITLE_SUBGRAPH );
+		graph.setNode( new Node( "Title_mid", false, getFilename( "Title", 250 ), "" ), node_id++, TITLE_SUBGRAPH );
+		graph.setNode( new Node( "Title_final_start", false, getFilename( "Title", 501 ), "" ), node_id++, TITLE_SUBGRAPH );
+		graph.setNode( new Node( "Title_final_end", true, getFilename( "Title", 550 ), "" ), node_id++, TITLE_SUBGRAPH );
 
 		{// 0 - 1
 			String[] filenames_for_0_to_1 = new String[ 249 ];
@@ -69,6 +79,22 @@ public class March2018LabMeeting {
 				filenames_for_2_to_3[ i ] = getFilename( "Title", 502 + i );
 			}
 			graph.addEdge( new Edge( "", 2, 3, filenames_for_2_to_3 ) );
+		}
+	}
+	
+	private static void setMRSNodesAndEdges( Graph graph, int offset ) {
+
+		int node_id = offset;
+		
+		graph.setNode( new Node( "Title", true, getFilename( "MRS", 60 ), "Good Luck!" ), node_id++, MRS_SUBGRAPH );
+		graph.setNode( new Node( "Title_end", false, getFilename( "MRS", 180 ), "" ), node_id++, MRS_SUBGRAPH );
+		
+		{// 0 - 1
+			String[] filenames_for_0_to_1 = new String[ 180 - 61 ];
+			for( int i = 0; i < filenames_for_0_to_1.length; ++i ) {
+				filenames_for_0_to_1[ i ] = getFilename( "MRS", 61 + i );
+			}
+			graph.addEdge( new Edge( "Wrecking Ball!", offset, offset + 1, filenames_for_0_to_1 ) );
 		}
 	}
 
