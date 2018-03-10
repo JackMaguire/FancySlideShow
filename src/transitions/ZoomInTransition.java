8c681a054db0f92dc19db019bedeb9289dcf2557
Jack Maguire <jackmaguire1444@gmail.com>
2018-03-09 20:20:13 -0500

    Created ZoomInTransition class but still need to write it

---
+++ src/transitions/ZoomInTransition.java
@@ -0,0 +1,12 @@
+package transitions;
+
+import graph.EdgeType;
+import graph.NodeType;
+
+public class ZoomInTransition {
+
+	public static EdgeType make( NodeType start, NodeType end, int num_frames ) {
+		return null;//TODO
+	}
+	
+}
