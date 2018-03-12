e033948642b474cf39578613d99059875a97eddb
Jack Maguire <jack@med.unc.edu>
2018-03-12 17:22:02 -0400

    nevermind about options

--- src/compile_time_settings/PerformanceSettings.java
+++ src/compile_time_settings/PerformanceSettings.java
@@ -7,7 +7,7 @@ public class PerformanceSettings {
 	public final static int PRI_MAX_WIDTH = 1024;
 	public final static int PRI_MAX_HEIGHT = 1024;
 
-	public final static double SECONDARY_NODE_CACHE_RATIO = 0.0;// 0 means use following:
+	public final static double SECONDARY_NODE_CACHE_RATIO = 1.0;// 0 means use following:
 	public final static int SEC_MAX_WIDTH = 1024;
 	public final static int SEC_MAX_HEIGHT = 1024;
 
