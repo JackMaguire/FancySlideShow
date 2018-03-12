14c1e6f82c1eb609d7614801c0f878db3ea7812c
Jack Maguire <jack@med.unc.edu>
2018-03-12 17:26:58 -0400

    Updated FrameRateMEasurer to be more useful

--- src/test/FrameRateMeasurer.java
+++ src/test/FrameRateMeasurer.java
@@ -4,7 +4,7 @@ public class FrameRateMeasurer {
 
 	// private final static FrameRateMeasurer instance_ = new FrameRateMeasurer();
 	private final static int num_measurements = 1024;
-	private final static int max_bin_ = 50;
+	private final static int max_bin_ = 100;
 
 	private final static long[] measurements_ = new long[ num_measurements ];
 	private static int id = 0;
@@ -45,5 +45,6 @@ public class FrameRateMeasurer {
 		for( int i = 0; i < max_bin_; ++i ) {
 			System.out.println( i + "\t" + counts_[ i ] );
 		}
+		System.exit( 0 );
 	}
 }
