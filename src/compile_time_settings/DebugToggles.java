bad36d4753777abc9db2ef5aee5b001f62e36be9
Jack Maguire <jack@med.unc.edu>
2018-03-12 14:21:48 -0400

    turned on debugging

--- src/compile_time_settings/DebugToggles.java
+++ src/compile_time_settings/DebugToggles.java
@@ -7,7 +7,7 @@ public class DebugToggles {
 	public final static boolean DEBUG_APPLICATIONS = DEBUG_ALL || false;
 	public final static boolean DEBUG_CONTROL_PANEL = DEBUG_ALL || false;
 	public final static boolean DEBUG_FRAME_GRAPH = DEBUG_ALL || false;
-	public final static boolean DEBUG_FRAME_RATE = DEBUG_ALL || false;
+	public final static boolean DEBUG_FRAME_RATE = DEBUG_ALL || true;
 	public final static boolean DEBUG_GRAPH = DEBUG_ALL || false;
 	public final static boolean DEBUG_KEYS = DEBUG_ALL || false;
 	public final static boolean DEBUG_SLIDE_SHOW = DEBUG_ALL || false;
