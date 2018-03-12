492b2f5440460c2524df72de278ab925ec838600
Jack Maguire <jackmaguire1444@gmail.com>
2018-03-12 15:57:57 -0400

    run.sh now has visualvm options

--- run.sh
+++ run.sh
@@ -5,6 +5,10 @@ slide_location=~/Dropbox/March2018LabMeetingSlides/
 
 temp=`echo $application | sed 's:/:.:g'`
 
+#compilation_flags=""
+compilation_flags="-Xverify:none" #VisualVM
+
+mkdir DOOMED
 javac -d DOOMED/ -cp src src/applications/${application}.java
-java -cp DOOMED applications.$temp $slide_location
+java $compilation_flags= -cp DOOMED applications.$temp $slide_location
 \rm -rf DOOMED
