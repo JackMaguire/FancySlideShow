02c78fff020f49222a4182d29aa5fb3775b3f4a6
Jack Maguire <jack@med.unc.edu>
2018-03-12 12:33:51 -0400

    Made run.sh more customizable

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-javac -d bin/ -cp src src/applications/*.java
+application="one_time/March2018LabMeeting"
+slide_location=~/Dropbox/March2018LabMeetingSlides/
+
+temp=`echo $application | sed 's:/:.:g'`
+
+javac -d DOOMED/ -cp src src/applications/${application}.java
+java -cp DOOMED applications.$temp $slide_location
+\rm -rf DOOMED
