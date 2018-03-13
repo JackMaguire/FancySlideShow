af76fd0e68ae87812849e342fd22ba5a4e8aaa95
Jack Maguire <jack@med.unc.edu>
2018-03-13 10:09:32 -0400

    PerformanceSettings

--- src/compile_time_settings/PerformanceSettings.java
+++ src/compile_time_settings/PerformanceSettings.java
@@ -3,14 +3,14 @@ package compile_time_settings;
 public class PerformanceSettings {
 
 	// Frame Caching
-	public final static double PRIMARY_NODE_CACHE_RATIO = 1.0;// 0 means use following:
-	public final static int PRI_MAX_WIDTH = 1024;
-	public final static int PRI_MAX_HEIGHT = 1024;
+	public final static double PRIMARY_NODE_CACHE_RATIO = 0;// 0 means use following:
+	public final static int PRI_MAX_WIDTH = 1920;
+	public final static int PRI_MAX_HEIGHT = 1080;
 
-	public final static double SECONDARY_NODE_CACHE_RATIO = 1.0;// 0 means use following:
-	public final static int SEC_MAX_WIDTH = 1024;
-	public final static int SEC_MAX_HEIGHT = 1024;
+	public final static double SECONDARY_NODE_CACHE_RATIO = 0.6;// 0 means use following:
+	public final static int SEC_MAX_WIDTH = 1920;
+	public final static int SEC_MAX_HEIGHT = 1080;
 
-	public final static boolean DELETE_CACHES = true;
-	public final static boolean LOAD_CACHES = false;
+	public final static boolean DELETE_CACHES = false;
+	public final static boolean LOAD_CACHES = true;
 }
