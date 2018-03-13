1f74c42d5f5ad4a5a2db24882dd89b296535fc6c
Jack Maguire <jack@med.unc.edu>
2018-03-13 11:33:16 -0400

    big pics

--- src/compile_time_settings/PerformanceSettings.java
+++ src/compile_time_settings/PerformanceSettings.java
@@ -8,8 +8,8 @@ public class PerformanceSettings {
 	public final static int PRI_MAX_HEIGHT = 1080;
 
 	public final static double SECONDARY_NODE_CACHE_RATIO = 0;// 0 means use following:
-	public final static int SEC_MAX_WIDTH = 1920 / 2;
-	public final static int SEC_MAX_HEIGHT = 1080 / 2;
+	public final static int SEC_MAX_WIDTH = 1920;
+	public final static int SEC_MAX_HEIGHT = 1080;
 
 	public final static boolean DELETE_CACHES = false;
 	public final static boolean LOAD_CACHES = false;
