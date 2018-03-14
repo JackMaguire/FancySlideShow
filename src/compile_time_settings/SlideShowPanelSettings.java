c41cafcee71cf2b655aad0f3518e45b0104415f8
Jack Maguire <jackmaguire1444@gmail.com>
2018-03-14 11:13:28 -0400

    Time to take a break

--- src/compile_time_settings/SlideShowPanelSettings.java
+++ src/compile_time_settings/SlideShowPanelSettings.java
@@ -2,7 +2,12 @@ package compile_time_settings;
 
 import java.awt.Color;
 
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+
 public class SlideShowPanelSettings {
+
+	public final static String XML_Name = "SlideShowPanel";
 
 	public static int MONITOR = 0;
 	// public final static int WIDTH = 1024;
@@ -11,4 +16,14 @@ public class SlideShowPanelSettings {
 	public static Color BACKGROUND = Color.BLACK;
 	// public final static int SCALE_PIC = true;
 
+	public static void parseXMLNode( Node xml_node ) {
+		final NamedNodeMap nodes = xml_node.getAttributes();
+		
+		final int n_elements = nodes.getLength();
+		for( int i = 0; i < n_elements; ++i ) {
+			final Node element = nodes.item( i );
+			System.out.println(  element.getNodeName() );
+			//TODO pick up from here please!
+		}
+	}
 }
--- src/xml_parsing/ParseSettings.java
+++ src/xml_parsing/ParseSettings.java
@@ -2,10 +2,25 @@ package xml_parsing;
 
 import org.w3c.dom.Node;
 
+import compile_time_settings.SlideShowPanelSettings;
+
 public class ParseSettings {
 
 	public static void parseSettingsNode( Node settings_node ) {
+		Node ptr = settings_node.getFirstChild();
+		Node last = settings_node.getLastChild();
 		
+		while( ptr != last ) {
+			parseNode( ptr );
+			ptr = ptr.getNextSibling();
+		}
+		parseNode( last );
+	}
+	
+	private static void parseNode( Node individual_node ) {
+		if( individual_node.getNodeName().equals( SlideShowPanelSettings.XML_Name ) ) {
+			SlideShowPanelSettings.parseXMLNode( individual_node );
+		}
 	}
 	
 }
