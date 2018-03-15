b5f3087c0adf58601d087d646031301b1eabc306
Jack Maguire <jackmaguire1444@gmail.com>
2018-03-15 14:20:13 -0400

    makeEdge Attributes

--- src/xml_parsing/ConceptualEdgeFactory.java
+++ src/xml_parsing/ConceptualEdgeFactory.java
@@ -1,7 +1,9 @@
 package xml_parsing;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 
 import conceptual_graph.ConceptualEdge;
@@ -40,10 +42,48 @@ public class ConceptualEdgeFactory {
 	}
 
 	private static ConceptualEdge makeEdge( Node edge_node, HashMap< String, Integer > local_index_for_node_title,
-			int node_offset, String filename_prefix ) {
+			int node_offset, String filename_prefix ) throws XMLParsingException {
+
+		String title = "";
+		int origin_node = -1;
+		int destination_node = -1;
 
 		// Attributes
+		final NamedNodeMap attribute_nodes = edge_node.getAttributes();
+		final int n_attributes = attribute_nodes.getLength();
+		for( int i = 0; i < n_attributes; ++i ) {
+			final Node attribute = attribute_nodes.item( i );
+			final String attribute_name = attribute.getNodeName();
+			final String value = attribute.getNodeValue();
 
+			if( attribute_name.equalsIgnoreCase( "title" ) ) {
+				title = value;
+			} else if( attribute_name.equalsIgnoreCase( "origin_node" ) ) {
+				if( !local_index_for_node_title.containsKey( value ) ) {
+					throw new XMLParsingException( "Edge can not find Node with name: " + value );
+				}
+				origin_node = node_offset + local_index_for_node_title.get( value );
+			} else if( attribute_name.equalsIgnoreCase( "destination_node" ) ) {
+				if( !local_index_for_node_title.containsKey( value ) ) {
+					throw new XMLParsingException( "Edge can not find Node with name: " + value );
+				}
+				destination_node = node_offset + local_index_for_node_title.get( value );
+			} else if( !attribute_name.startsWith( "#" ) ) {
+				System.err.println( "Edge has no match for " + attribute_name );
+				throw new XMLParsingException( "Edge has no match for " + attribute_name );
+			}
+		}
+
+		if( origin_node == -1 ) {
+			throw new XMLParsingException( "Edge has no origin node: " + title );
+		}
+		
+		if( destination_node == -1 ) {
+			throw new XMLParsingException( "Edge has no destination node: " + title );
+		}
+		
+		final ArrayList< String > frame_filenames = new ArrayList< String >();
+		
 		return null;// TODO
 	}
 
