d4842b96d7050c96d60b4c679256aa95ae629dcd
Jack Maguire <jackmaguire1444@gmail.com>
2018-03-15 09:10:48 -0400

    SlideShowPanel now parses some xml

--- src/compile_time_settings/SlideShowPanelSettings.java
+++ src/compile_time_settings/SlideShowPanelSettings.java
@@ -16,14 +16,26 @@ public class SlideShowPanelSettings {
 	public static Color BACKGROUND = Color.BLACK;
 	// public final static int SCALE_PIC = true;
 
+	public static int FPS = 24;
+	
 	public static void parseXMLNode( Node xml_node ) {
+		
+		//Attributes
 		final NamedNodeMap nodes = xml_node.getAttributes();
 
 		final int n_elements = nodes.getLength();
 		for( int i = 0; i < n_elements; ++i ) {
 			final Node element = nodes.item( i );
-			System.out.println( element.getNodeName() );
-			// TODO pick up from here please!
+			System.out.println( element.getNodeName() + " " + element.getNodeValue() );
+			if( element.getNodeName().equalsIgnoreCase( "fps" ) ) {
+				FPS = Integer.parseInt( element.getNodeValue() );
+			} else if( element.getNodeName().equalsIgnoreCase( "monitor" ) ) {
+				MONITOR = Integer.parseInt( element.getNodeValue() );
+			}
 		}
+	}
+	
+	private static void parseColor( Node color_node ) {
+		// TODO pick up from here please!
 	}
 }
