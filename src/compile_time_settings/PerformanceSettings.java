562e13933a270f1a91ab854dd10ad4dcb35e46ec
Jack Maguire <jackmaguire1444@gmail.com>
2018-03-15 11:03:59 -0400

    PerformanceSettings has xml template

--- src/compile_time_settings/PerformanceSettings.java
+++ src/compile_time_settings/PerformanceSettings.java
@@ -1,6 +1,11 @@
 package compile_time_settings;
 
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+
 public class PerformanceSettings {
+
+	public final static String XML_Name = "FrameCache";
 
 	// Frame Caching
 	public final static double PRIMARY_NODE_CACHE_RATIO = 0;// 0 means use following:
@@ -13,4 +18,38 @@ public class PerformanceSettings {
 
 	public final static boolean DELETE_CACHES = false;
 	public final static boolean LOAD_CACHES = false;
+
+	public static void parseXMLNode( Node xml_node ) {
+
+		// Attributes
+		final NamedNodeMap attribute_nodes = xml_node.getAttributes();
+		final int n_attributes = attribute_nodes.getLength();
+		for( int i = 0; i < n_attributes; ++i ) {
+			final Node attribute = attribute_nodes.item( i );
+			final String attribute_name = attribute.getNodeName();
+			final int value = Integer.parseInt( attribute.getNodeValue() );
+
+			/*if( attribute_name.equalsIgnoreCase( "monitor" ) ) {
+				MONITOR = value;
+			} else if( attribute_name.equalsIgnoreCase( "width" ) ) {
+				CP_WIDTH = value;
+			} else if( attribute_name.equalsIgnoreCase( "height" ) ) {
+				CP_HEIGHT = value;
+			} else if( attribute_name.equalsIgnoreCase( "east_width" ) ) {
+				EAST_WIDTH = value;
+			} else if( attribute_name.equalsIgnoreCase( "west_width" ) ) {
+				WEST_WIDTH = value;
+			}*/
+		}
+
+		/*
+		final NodeList element_nodes = xml_node.getChildNodes();
+		final int n_elements = element_nodes.getLength();
+		for( int i = 0; i < n_elements; ++i ) {
+			final Node element = element_nodes.item( i );
+			final String element_name = element.getNodeName();
+		
+		}
+		*/
+	}
 }
