2f97e4a798a42f01bb591bd954d3a1a46a75de8a
Jack Maguire <jackmaguire1444@gmail.com>
2018-03-16 17:28:54 -0400

    FrameScripts seems to work

--- src/applications/FrameScript.java
+++ src/applications/FrameScript.java
@@ -1,11 +1,8 @@
 package applications;
 
 import java.io.File;
-import java.io.IOException;
 
-import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
 import conceptual_graph.ConceptualGraph;
 import slide_show.SlideShow;
