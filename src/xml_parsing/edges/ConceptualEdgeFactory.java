1e5540bf488a959f8dc9543a6e46a3e7cd51da5d
Jack Maguire <jackmaguire1444@gmail.com>
2018-03-16 10:41:48 -0400

    ConceptualEdge.create() now calls DefaultEdgeCreator.makeGlobalEdge

--- src/xml_parsing/edges/ConceptualEdgeFactory.java
+++ src/xml_parsing/edges/ConceptualEdgeFactory.java
@@ -23,14 +23,30 @@ public class ConceptualEdgeFactory {
 		return false;
 	}
 
-	public static ConceptualEdge create( Node edge_node, ArrayList< FrameSpaceParser > parsed_frame_spaces ) throws XMLParsingException {
+	public static ConceptualEdge create( Node edge_node, ArrayList< FrameSpaceParser > parsed_frame_spaces,
+			int[] offset_for_frame_space ) throws XMLParsingException {
+		
+		final String node_name = edge_node.getNodeName();
+		if( node_name.equalsIgnoreCase( "Edge" ) ) {
+			return DefaultEdgeCreator.makeGlobalEdge( edge_node, parsed_frame_spaces, offset_for_frame_space );
+		} else if( node_name.equalsIgnoreCase( "ZoomInTransition" ) ) {
+			//return makeZoomInTransition( edge_node, local_index_for_node_title, node_offset );
+		} else {
+			if( XMLNameIsEdgeType( node_name ) ) {
+				System.err.println( "ConceptualEdgeFactory::xml_names_ is out of date: " + node_name );
+				System.exit( 1 );
+			} else {
+				throw new XMLParsingException( "ConceptualEdgeFactory: " + node_name + " is undefined." );
+			}
+		}
+		
 		return null;
 	}
-	
+
 	public static ConceptualEdge create( Node edge_node, HashMap< String, Integer > local_index_for_node_title,
 			int node_offset, String filename_prefix ) throws XMLParsingException {
+		
 		final String node_name = edge_node.getNodeName();
-
 		if( node_name.equalsIgnoreCase( "Edge" ) ) {
 			return DefaultEdgeCreator.makeEdge( edge_node, local_index_for_node_title, node_offset, filename_prefix );
 		} else if( node_name.equalsIgnoreCase( "ZoomInTransition" ) ) {
@@ -46,8 +62,6 @@ public class ConceptualEdgeFactory {
 
 		return null;
 	}
-
-
 
 	private static ConceptualEdge makeZoomInTransition( Node edge_node,
 			HashMap< String, Integer > local_index_for_node_title, int node_offset ) {
