1832d374eccad47e087da8fb9f8f7a9256944792
Jack Maguire <jackmaguire1444@gmail.com>
2018-03-16 17:02:40 -0400

    Added a directory from run scripts

---
+++ run/run_script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+application="one_time/March2018LabMeeting2"
+slide_location=~/Dropbox/March2018LabMeetingSlides/
+
+temp=`echo $application | sed 's:/:.:g'`
+
+compilation_flags=""
+#compilation_flags="-Xverify:none" #VisualVM
+
+mkdir DOOMED
+javac -d DOOMED/ -cp src src/applications/${application}.java
+java $compilation_flags -cp DOOMED applications.$temp $slide_location
+\rm -rf DOOMED
